cfc24001f0c5210209ad9f3f26775271721f6fb4
dspyrydo <dspyrydo@student.unit.ua>
2018-10-06 16:17:10 +0300

    fix map and guide

--- src/components/guide.jsx
+++ src/components/guide.jsx
@@ -6,7 +6,6 @@ const Panel = Collapse.Panel
 
 const Guide = () => (
   <div>
-  <h2>Dashboard</h2>
   <Collapse accordion>
     <Panel header="Repeat Visitors" key="1">
       <p>Shows repeat visitors by hour or by day. You can see the following repeat visitor categories:</p>
@@ -35,7 +34,6 @@ const Guide = () => (
       <p>- Shows information such as those pertaining to passersby, visitors, and connected devices, by hour (if it is a single day or last 3 days), or by day, for the given site.</p>
     </Panel>
   </Collapse>
-  <h2>Maps</h2>
   <Collapse accordion>
   <Panel header="Maps" key="1">
     <p>- Locate the visitors on the map in the real time;</p>
@@ -43,7 +41,6 @@ const Guide = () => (
     <p>- Get detailed information about particular visitor;</p>
   </Panel>
   </Collapse>
-  <h2>Forecast</h2>
   <Collapse accordion>
   <Panel header="Forecast" key="1">
     <p>- Get the forecast for the week ahead based on the previous 2 months historial data.</p>
